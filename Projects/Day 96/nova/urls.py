19265f2b63ed94a256269fd8c31e21a6d500d015
Preet Taparia <preettaparia@jklu.edu.in>
2023-09-18 23:29:44 +0530

    Add files via upload

---
+++ Projects/Day 96/nova/urls.py
@@ -0,0 +1,39 @@
+"""
+URL configuration for social project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('profile_list/', views.profile_list, name="profile_list"),
+    path('profile/<int:pk>', views.profile, name="profile"),
+    path('profile/followers/<int:pk>', views.followers, name="followers"),
+    path('profile/following/<int:pk>', views.following, name="following"),
+    path('login/', views.login_user, name="login"),
+    path('logout/', views.logout_user, name="logout"),
+    path('register/', views.register_user, name="register"),
+    path('update_user/', views.update_user, name="update_user"),
+    path('post_like/<int:pk>', views.post_like, name="post_like"),
+    path('post_show/<int:pk>', views.post_show, name="post_show"),
+    path('unfollow/<int:pk>', views.unfollow, name="unfollow"),
+    path('follow/<int:pk>', views.follow, name="follow"),
+    path('delete_post/<int:pk>', views.delete_post, name="delete_post"),
+    path('edit_post/<int:pk>', views.edit_post, name="edit_post"), 
+    path('search_post/', views.search_post, name="search_post"),
+    path('search_user/', views.search_user, name="search_user"), 
+    
+]
